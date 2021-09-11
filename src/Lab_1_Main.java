bba0b117b157270d28d9063a2eaa92937444ea64
goldenmaplesoftware <baronjac@live.ca>
2021-09-11 18:45:42 -0400

    Created the bank in which populates the word list,  then created the sub array that populates the user inputted guesses.  User input has been implemented."

--- src/Lab_1_Main.java
+++ src/Lab_1_Main.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Lab_1_Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Lab_1_Main
+{
+
+
+
+
+    ///This is where all the action happens
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> sample = new ArrayList();
+        sample.add(29);
+        sample.add(28);
+        sample.add(2);
+        sample.add(0);
+        Collections.sort(sample);
+        System.out.println(sample);
+        String[] wordBank = new String[]{"write", "that", "banana", "hollywood", "tinman"};
+        String randomWord = wordBank[(int)(Math.random() * (double)wordBank.length)];
+        System.out.println();
+        System.out.println("___________________________");
+        System.out.println("This word has " + randomWord.length() + " letters.");
+        char[] letters = new char[randomWord.length()];
+        Arrays.fill(letters, '*');
+        System.out.println("****HANG MAN********");
+        System.out.println("___________________________");
+        System.out.println("JEAN JACOB COMTOIS--- A GAME OF HANGMAN 2021\n");
+        System.out.println("Input a letter  to guess the word (lowercase only)");
+        Scanner inputLetter = new Scanner(System.in);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
